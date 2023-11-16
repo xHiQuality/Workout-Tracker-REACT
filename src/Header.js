a3dc839410b73faeaf5c720f97d43f49403ebdc7
Ethan Schrack <ethanschrack@gmail.com>
2023-11-16 16:24:38 -0500

    Render title and login button

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,13 @@
 import './Header.css'
 
-
+const Hdr = ({ }) => {
+      
+    return (
+      <div className="header">
+       <h1>Fitness App</h1>
+       <Button type="login">Login</Button>
+      </div>
+    );
+  };
 
 export default Header;
