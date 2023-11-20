8096cfb9b68026e9d829a0da4f44f6bc0e6dec87
Ethan Schrack <ethanschrack@gmail.com>
2023-11-20 14:05:30 -0500

    Work on login button in hdr.js to render login form

--- src/components/Hdr.js
+++ src/components/Hdr.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { useState } from 'react';
 import './Hdr.css';
 import Button from './Button';
+import Login from './Login';
+import { useState } from 'react';
 
-const Hdr = ({ onLogin }) => {
+const Hdr = ({onLogin}) => {
 
-  const [isFormVisible, setIsFormVisible] = useState(false); // New state variable
+  const handleLoginClick = () => {
+    onLogin();
+  };
 
   return (
     <div className="hdr">
      <h1>Workout Plus</h1>
-     <Button>Login</Button>
+     <Button type="submit" onClick={handleLoginClick}>Login</Button>
+     
     </div>
   );
 };
--- src/components/Login.js
+++ src/components/Login.js
@@ -4,19 +4,48 @@ import Button from './Button';
 import { useState } from 'react';
 
 const Login = ({onLogin}) => {
+
+    const [isFormVisible, setIsFormVisible] = useState(false); // New state variable
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const loginHandler = (event) => {
+        event.preventDefault();
+
+        const loggedInUser = {
+            username: username,
+            password: password
+        }
+
+        onLogin(loggedInUser);
+
+        setUsername('');
+        setPassword('');
+        setIsFormVisible(false);
+
+    }
+
+    const showFormHandler = () => {
+        setIsFormVisible(true)
+    }
+
     return (
         <div className='login'>
             <h1>Log In</h1>
-            <form>
+            <form onSubmit={loginHandler}>
                 <input
                     placeholder='Username'
                     name="username"
                     type="text"
+                    value={username.username}
+                    onChange={setUsername}
                 /> 
                 <input
                     placeholder='Password'
                     name="password"
                     type="password"
+                    value={password.password}
+                    onChange={setPassword}
                 />
                 <Button type="submit">Login</Button>
             </form>
