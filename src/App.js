04b8e745ef7f483cb1acdeab0c03d95ce5ecb921
Ethan Schrack <ethanschrack@gmail.com>
2023-11-20 11:03:22 -0500

    Set up routes in app.js and install react-router-dom

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Workout from './components/Workout_Section/Workout.js'
 import './WorkoutPage.css'; // Import your CSS file for styling
 import Summary from './components/Summary.js';
 import Hdr from './components/Hdr.js';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 
 function App() {
@@ -21,15 +22,36 @@ function App() {
     setUsers((prevUsers) => [...prevUsers, user]);
   };
 
+  function ErrorPage () {
+    return (
+      <div>
+        <h3>Error</h3>
+        <p>Page not found.</p>
+      </div>
+    )
+  }
+
   return (
-    <div>
-      <Hdr />
-      <Summary />
-      <AddUser onAddUser={addUserHandler} />
-      <UserList users={users} />
-    
-    
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path='/' element={
+            <React.Fragment>
+              <Hdr />
+              <UserList users={users} /* This should be a dummy array of users initially */ />
+            </React.Fragment>
+          } />
+          <Route path='user-auth' element={
+            <React.Fragment>
+              <Hdr />
+              <UserList users={users} />
+              <AddUser onAddUser={addUserHandler} />
+            </React.Fragment>
+          } />
+          <Route path='*' element={<ErrorPage />}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
