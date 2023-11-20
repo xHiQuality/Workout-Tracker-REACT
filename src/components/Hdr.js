92701e67d4c659b83ee3b61e8aad37ad36ae8c19
Ethan Schrack <ethanschrack@gmail.com>
2023-11-20 10:11:28 -0500

    Merge header and hdr into one component (hdr)

--- src/components/Hdr.js
+++ src/components/Hdr.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import './Hdr.css';
+import Button from './Button';
 
 const Hdr = () => {
   return (
     <div className="hdr">
      <h1>Workout Plus</h1>
+     <Button>Login</Button>
     </div>
   );
 };
