8e02a18aa4076bd41ae063a02ad9f3b7852198ba
Ethan Schrack <ethanschrack@gmail.com>
2023-11-20 21:54:52 -0500

    Merge conflicts

--- src/components/Hdr.js
+++ src/components/Hdr.js
@@ -1,22 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import './Hdr.css';
-import Button from './Button';
-import Login from './Login';
-import { useState } from 'react';
-
-const Hdr = ({onLogin}) => {
-
-  const handleLoginClick = () => {
-    onLogin();
-  };
-
-  return (
-    <div className="hdr">
-     <h1>Workout Plus</h1>
-     <Button type="submit" onClick={handleLoginClick}>Login</Button>
-     
-=======
 import { useState } from 'react';
 
 import './Hdr.css';
@@ -38,7 +20,6 @@ const Hdr = () => {
         {showLogin ? 'Stay logged out' : 'Login'}
       </Button>
       {showLogin && <Login />}
->>>>>>> main
     </div>
   );
 };
--- src/components/Login.js
+++ src/components/Login.js
@@ -6,44 +6,15 @@ import { Link } from 'react-router-dom';
 
 const Login = ({onLogin}) => {
 
-<<<<<<< HEAD
-    const [isFormVisible, setIsFormVisible] = useState(false); // New state variable
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-
-    const loginHandler = (event) => {
-        event.preventDefault();
-
-        const loggedInUser = {
-            username: username,
-            password: password
-        }
-
-        onLogin(loggedInUser);
-
-        setUsername('');
-        setPassword('');
-        setIsFormVisible(false);
-
-    }
-
-    const showFormHandler = () => {
-        setIsFormVisible(true)
-=======
     const submitHandler = (e) => {
         e.preventDefault();
         console.log('Login displayed');
->>>>>>> main
     }
 
     return (
         <div className='login'>
             <h1>Log In</h1>
-<<<<<<< HEAD
-            <form onSubmit={loginHandler}>
-=======
             <form onSubmit={submitHandler}>
->>>>>>> main
                 <input
                     placeholder='Username'
                     name="username"
@@ -54,13 +25,7 @@ const Login = ({onLogin}) => {
                 <input
                     placeholder='Password'
                     name="password"
-<<<<<<< HEAD
-                    type="password"
-                    value={password.password}
-                    onChange={setPassword}
-=======
                     type="text"
->>>>>>> main
                 />
                 <Button type="submit">
                     <Link to="/user-auth">Login</Link> 
