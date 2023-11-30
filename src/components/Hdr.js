aff7abdc800380c7f53f50fdc54c388022a25d3f
Ethan Schrack <ethanschrack@gmail.com>
2023-11-29 22:44:56 -0500

    Add logo to Hdr only

--- src/components/Hdr.js
+++ src/components/Hdr.js
@@ -4,8 +4,11 @@ import { useState } from 'react';
 import './Hdr.css';
 import Button from './Button';
 import Login from './Login';
+import trackerLogo from './images/workout-tracker--high-resolution-logo-transparent.png';
 
 const Hdr = () => {
+
+  console.log(trackerLogo)
 
   const [showLogin, setShowLogin] = useState(false);
 
@@ -15,8 +18,10 @@ const Hdr = () => {
 
   return (
     <div className="hdr">
-      <h1>Workout Tracker +</h1>
-      {/* <img src="images/workout-tracker--high-resolution-logo-transparent.png" alt='Workout Tracker + Logo'/> */}
+      <div>
+        <img src='./static/media/workout-tracker--high-resolution-logo-transparent.ed2d488200da2f997557.png' alt='logo' className='logo'/> 
+        <h1>Workout Tracker +</h1>
+      </div>
       <Button className="button" type="submit" onClick={handleLogin}>
         {showLogin ? 'Stay logged out' : 'Login'}
       </Button>
