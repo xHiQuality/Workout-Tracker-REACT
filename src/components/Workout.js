9ff3bd387f60fb9fd16d01750872df09f9be994c
Cason Pittman <xhiquality@gmail.com>
2023-11-29 21:55:18 -0500

    Logic for displaying buttons based on route

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Food.css';
 import Card from './Card';
+import Button from './Button';
 
 const Workout = props => {
     return (    
@@ -8,6 +10,15 @@ const Workout = props => {
         <li key={props.id} className="user-item">
         <div className="user-info">
             <h2>Exercise:</h2>
+
+            {window.location.pathname === '/user-auth' && (
+                <Link to ='/*'>
+                    <Button type = 'submit' id = 'edit'>
+                        Edit
+                    </Button>
+                </Link>
+            )}
+
             <h3>Workout: {props.workout}</h3>
             <h3>Calories: {props.calories}</h3>
         </div>
